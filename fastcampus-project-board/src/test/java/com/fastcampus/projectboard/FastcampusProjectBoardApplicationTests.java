dcd9dd29c30aa3a62f0980b65a639c0a53d293b7
Jim3-4 <kimjm0304@naver.com>
2022-10-23 14:59:51 +0900

    #18 업데이트

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/FastcampusProjectBoardApplicationTests.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/FastcampusProjectBoardApplicationTests.java
@@ -2,12 +2,15 @@ package com.fastcampus.projectboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootTest
 class FastcampusProjectBoardApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
