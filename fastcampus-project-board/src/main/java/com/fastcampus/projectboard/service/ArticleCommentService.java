0010a481823a66ff2ffee25546a16c787df3aa34
Jim3-4 <kimjm0304@naver.com>
2022-10-26 17:59:15 +0900

    #20 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    강의에서 보여주지 않은 나머지 로직의 테스트 코드를 작성하고
    연관된 DTO 를 재설계함
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleCommentService.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleCommentService.java
@@ -1,6 +1,5 @@
 package com.fastcampus.projectboard.service;
 
-
 import com.fastcampus.projectboard.dto.ArticleCommentDto;
 import com.fastcampus.projectboard.repository.ArticleCommentRepository;
 import com.fastcampus.projectboard.repository.ArticleRepository;
@@ -19,11 +18,17 @@ public class ArticleCommentService {
     private final ArticleCommentRepository articleCommentRepository;
 
     @Transactional(readOnly = true)
-    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+    public List<ArticleCommentDto> searchArticleComments(Long articleId) {
         return List.of();
     }
 
     public void saveArticleComment(ArticleCommentDto dto) {
+    }
+
+    public void updateArticleComment(ArticleCommentDto dto) {
+    }
+
+    public void deleteArticleComment(Long articleCommentId) {
     }
 
 }
--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
@@ -1,17 +1,13 @@
 package com.fastcampus.projectboard.service;
 
-
 import com.fastcampus.projectboard.domain.type.SearchType;
 import com.fastcampus.projectboard.dto.ArticleDto;
-import com.fastcampus.projectboard.dto.ArticleUpdateDto;
 import com.fastcampus.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
@@ -21,17 +17,22 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
+    @Transactional(readOnly = true)
+    public ArticleWithCommentsDto getArticle(Long articleId) {
+        return null;
+    }
+
     public void saveArticle(ArticleDto dto) {
-
     }
 
-    public void updateArticle(long l, ArticleUpdateDto of) {
+    public void updateArticle(ArticleDto dto) {
     }
 
-    public void deleteArticle(long l) {
+    public void deleteArticle(long articleId) {
     }
+
 }
